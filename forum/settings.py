9048b81d88e5e57139ad81e488ecbbb7c4d0dd1f
jokotoye18 <jokotoyeademola95@gmail.com>
2020-07-10 01:40:24 +0100

    martor intregration inplace of django-ckeditor

--- forum/settings.py
+++ forum/settings.py
@@ -141,9 +141,9 @@ DATABASES = {
     }
 }
 
-# DATABASE_URL = config('DATABASE_URL')
-# db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-# DATABASES['default'].update(db_from_env)
+DATABASE_URL = config('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 
 SITE_ID = 1
@@ -155,7 +155,7 @@ MARTOR_ENABLE_CONFIGS = {
     'imgur': 'false',        # to enable/disable imgur/custom uploader.
     'mention': 'true',     # to enable/disable mention
     'jquery': 'true',       # to include/revoke jquery (require for admin default django)
-    'living': 'false',      # to enable/disable live updates in preview
+    'living': 'true',      # to enable/disable live updates in preview
     'spellcheck': 'false',  # to enable/disable spellcheck in form textareas
     'hljs': 'true',         # to enable/disable hljs highlighting in preview
 }
@@ -313,7 +313,7 @@ if ENVIRONMENT == 'production':
     SESSION_COOKIE_HTTPONLY = True
     #django-csp(Details at official docs)
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS':	(
